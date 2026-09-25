0e8662e406c0691f8cb300b682328fd87c47dcfa
Stylish-Dark <Mr.Nick.Samuel@gmail.com>
2026-09-25 19:20:30 +1000

    Expose background stage preprocessing context

--- port/include/preprocess.h
+++ port/include/preprocess.h
@@ -7,6 +7,8 @@ typedef u8 *(*preprocessfunc)(u8 *data, u32 size, u32 *outSize);
 
 void preprocessSetFileNum(s32 fileNum);
 s32 preprocessGetFileNum(void);
+void preprocessSetBgStage(s32 stageNum);
+s32 preprocessGetBgStage(void);
 
 u8* preprocessAnimations(u8 *data, u32 size, u32 *outSize);
 u8* preprocessMpConfigs(u8 *data, u32 size, u32 *outSize);
