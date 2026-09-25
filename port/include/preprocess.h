3e28da533e07b0503dc1d169d2dab150f79a8703
Stylish-Dark <Mr.Nick.Samuel@gmail.com>
2026-09-25 19:54:08 +1000

    Track environment texture loading context

--- port/include/preprocess.h
+++ port/include/preprocess.h
@@ -9,6 +9,8 @@ void preprocessSetFileNum(s32 fileNum);
 s32 preprocessGetFileNum(void);
 void preprocessSetBgStage(s32 stageNum);
 s32 preprocessGetBgStage(void);
+void preprocessSetEnvironmentTextureLoad(s32 enabled);
+s32 preprocessGetEnvironmentTextureLoad(void);
 
 u8* preprocessAnimations(u8 *data, u32 size, u32 *outSize);
 u8* preprocessMpConfigs(u8 *data, u32 size, u32 *outSize);
