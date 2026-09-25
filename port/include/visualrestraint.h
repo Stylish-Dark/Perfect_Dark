68155d6a7a310f338a8b1e5a95ce6d4bc70439fe
Stylish-Dark <Mr.Nick.Samuel@gmail.com>
2026-09-25 19:53:29 +1000

    Add shared visual restraint stage profiles

---
+++ port/include/visualrestraint.h
@@ -0,0 +1,19 @@
+#ifndef PORT_VISUAL_RESTRAINT_H
+#define PORT_VISUAL_RESTRAINT_H
+
+#include <PR/ultratypes.h>
+
+enum visualrestraintstageprofile {
+	VISUAL_RESTRAINT_STAGE_NONE,
+	VISUAL_RESTRAINT_STAGE_GROUNDED,
+	VISUAL_RESTRAINT_STAGE_DATADYNE,
+	VISUAL_RESTRAINT_STAGE_G5,
+	VISUAL_RESTRAINT_STAGE_MARITIME,
+	VISUAL_RESTRAINT_STAGE_AREA51,
+	VISUAL_RESTRAINT_STAGE_AVIATION,
+};
+
+enum visualrestraintstageprofile visualRestraintGetStageProfile(s32 stage);
+void visualRestraintApplyEnvironmentPixel(enum visualrestraintstageprofile profile, u8 *r, u8 *g, u8 *b);
+
+#endif
